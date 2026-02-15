05c98c700542cc07a844b57da48897c5e89d90aa
yonas <yonas@users.noreply.github.com>
2026-02-15 06:13:46 +0000

    deploy: 26a5609e3806e84e4c5055a1efec0681128b2095

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-703a2a57.js',
+                path_to_root + 'searchindex-ffbd9b05.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
